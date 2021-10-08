93918afe4fc71b2809a1e25384d32284de5410c5
EC2 Default User <ec2-user@ip-172-16-73-53.ap-northeast-1.compute.internal>
2021-10-08 15:20:39 +0000

    small fix

--- src/lmp_equiliv.sh
+++ src/lmp_equiliv.sh
@@ -3,6 +3,18 @@
 export CUDA_VISIBLE_DEVICES="0"
 export OMPI_ALLOW_RUN_AS_ROOT=1
 
-mpirun --allow-run-as-root -np 2 /program/lammps201029/build/lmp -sf gpu -pk gpu 1 -in lmp_equiliv.in > lmp_equiliv.log 2>&1
 
-python lmp2data.py 'equiliv' >> lmp2data_equiliv.log 2>&1
+LMPLOGFILE="lmp_equiliv.log"
+PYLOGFILE="lmp_equiliv.log"
+ 
+if [ -e $LMPLOGFILE ]; then
+  rm $LMPLOGFILE
+fi
+
+if [ -e $PYLOGFILE ]; then
+  rm $PYLOGFILE
+fi
+
+mpirun --allow-run-as-root -np 2 /program/lammps201029/build/lmp -sf gpu -pk gpu 1 -in lmp_equiliv.in |& tee $LMPLOGFILE
+
+python lmp2data.py 'equiliv' |& tee $PYLOGFILE
