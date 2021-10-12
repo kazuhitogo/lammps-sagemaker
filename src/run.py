2aad2229a59b7bb33ccbbc427901ea1ec0eaf01c
EC2 Default User <ec2-user@ip-172-16-79-223.ap-northeast-1.compute.internal>
2021-10-12 14:28:24 +0000

    add comment]

--- src/run.py
+++ src/run.py
@@ -4,7 +4,7 @@ import subprocess, argparse, os, stat, shutil
 LOGFILE_NAME = 'lmp_equiliv.log'
 LMP_PATH = '/program/lammps201029/build/lmp'
 
-# Set environment variables
+# 環境変数の設定
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ['OMPI_ALLOW_RUN_AS_ROOT'] = '1'
 os.environ['OMP_NUM_THREADS'] = '1'  
@@ -29,28 +29,36 @@ if __name__=='__main__':
     logfile_tmp_path = os.path.join(args.input_dir,LOGFILE_NAME)
     logfile_path = os.path.join(args.output_dir,LOGFILE_NAME)
     
+    # デフォルトだとカレントディレクトリは root であり、
+    # mpirunに都合が悪いのでファイル群が置いてあるinput_dir(/opt/ml/processing/input)に移動
     # cd /opt/ml/processing/input/
+    # ※このコードは/opt/ml/processing/input/code/以下にある
+    print('change directory')
     os.chdir(input_dir)
     
     print('check current directory')
-    cmd_response = subprocess.run(["ls", "-l"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cmd_response = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print(cmd_response.stdout.decode())
-    
+        
     print('mpi will be started with following command.')
     command = f'bash {args.input_equiliv_sh} {args.np} {args.gpu} {LMP_PATH} {args.input_equiliv_in} {logfile_tmp_path}'
     command_list = command.split(' ')
     print(*command_list)
     cmd_response = subprocess.run(command_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    with open(os.path.join(args.output_dir,'dump.txt'),mode='w') as f:
+    # mpirun を走らせると大量の標準エラー出力が出るので、ダンプして output_dir に置く(併せて標準出力も)
+    # output_dir(/opt/ml/processing/output/) に置くと処理が完了したあと自動的に S3 に転送される
+    with open(os.path.join(args.output_dir,'mpirun_stdout.txt'),mode='w') as f:
         f.write(cmd_response.stdout.decode())
+    with open(os.path.join(args.output_dir,'mpirun_stderr.txt'),mode='w') as f:
+        f.write(cmd_response.stderr.decode())
     print('end.')
 
-    # copy generated files to output directory 
+    # mpirun を実行して生成されるファイルを output_dir にコピーしておく
     for file_name in ['lmp_equiliv.lammpstrj','lmp_equiliv.log','log.cite','log.lammps']:
         shutil.copy2(os.path.join(args.input_dir,file_name), args.output_dir)
     
     
-    # lmp2data.py exec
+    # lmp2data.py を実行
     print('exec lmp2data.py with following command.')
     command = f'python {args.input_lmp2data_py} equiliv'
     command_list = command.split(' ')
@@ -59,12 +67,12 @@ if __name__=='__main__':
     print('end.')
     
     # lmp2data.py の結果をテキストファイルに書き込み
-    with open(os.path.join(args.output_dir,'stdout.txt'),mode='w') as f:
+    with open(os.path.join(args.output_dir,'lmp2data_stdout.txt'),mode='w') as f:
         f.write(cmd_response.stdout.decode())
-    with open(os.path.join(args.output_dir,'stderr.txt'),mode='w') as f:
+    with open(os.path.join(args.output_dir,'lmp2data_stderr.txt'),mode='w') as f:
         f.write(cmd_response.stderr.decode())
     
-    # 併せて標準出力(NotebookとCWLへ書き込み)
+    # 併せて標準出力(Notebookの出力とCWLへ書き込み)
     print('stdout:')
     print(cmd_response.stdout.decode())
     print('stderr:')
